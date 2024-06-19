0fff32f000a9060247c943e9e37def0dc975b67b
Lucas M. Angelo <angelucas@gmail.com>
2024-06-19 20:15:26 -0300

    Reorganization of files in the directories to add a new test project to the solution

---
+++ Folder4files/Helpers/DirectoryOperations.cs
@@ -0,0 +1,51 @@
+﻿namespace Folder4Files.Helpers
+{
+    public static class DirectoryOperations
+    {
+        public static int MoveFileToNewDirectory(string file, string directory, DirectoryInfo newDirectory)
+        {
+            string newDirectoryPath = newDirectory.FullName;
+            string fileNameWithExtension = Path.GetFileName(file);
+            string detinationFilePath = Path.Combine(newDirectoryPath, fileNameWithExtension);
+
+            if (FileOperations.ShouldMoveFileToNewDirection(file, directory))
+            {
+                try
+                {
+                    if (!File.Exists(detinationFilePath))
+                    {
+                        File.Move(file, detinationFilePath);
+
+                        if (File.Exists(detinationFilePath))
+                            ConsoleOperations.WriteSuccess($"The file {fileNameWithExtension} has been moved!\n");
+                        else
+                            ConsoleOperations.WriteError($"Failed to moving file: {fileNameWithExtension}");
+                    }
+                    else
+                    {
+                        ConsoleOperations.WriteWarning($"There is already a folder {newDirectoryPath} containing the file '{fileNameWithExtension}'.\n");
+                        ConsoleOperations.WriteWarning($"The file '{file}' was not been moved.\n");
+                        return 0;
+                    }
+                }
+                catch (IOException ex)
+                {
+                    ConsoleOperations.WriteError($"Error moving file '{fileNameWithExtension}'. Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                ConsoleOperations.WriteError($"The file '{fileNameWithExtension}' or '{newDirectoryPath}' folder does not exist in '{Program.RootFolderPath}'.");
+            }
+            return 1;
+        }
+
+        public static DirectoryInfo CreateNewDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+                return Directory.CreateDirectory(path);
+
+            return new DirectoryInfo(path);
+        }
+    }
+}
---
+++ Folder4files/Helpers/FileOperations.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Folder4Files.Helpers
+{
+    internal static class FileOperations
+    {
+        private const string DiscInfoPattern = @" \(Disc \d+\)";
+
+        public static void ProcessFiles()
+        {
+            int filesMoved = 0;
+
+            foreach (string file in Program.Files)
+            {
+                string rawFileName = RemoveDiscInfo(Path.GetFileNameWithoutExtension(file));
+
+                if (ShouldMoveFile(file))
+                {
+                    ConsoleOperations.WriteInfo($"Processing {rawFileName} file transfer...");
+                    DirectoryInfo newDirectory = DirectoryOperations.CreateNewDirectory(rawFileName);
+                    filesMoved += DirectoryOperations.MoveFileToNewDirectory(file, rawFileName, newDirectory);
+                }
+                else if (!IsIgnoredFile(file))
+                {
+                    ConsoleOperations.WriteWarning($"That path already exists: {Path.Combine(Program.RootFolderPath, rawFileName)}");
+                }
+            }
+
+            ConsoleOperations.WriteWarning($"{filesMoved} files were moved!");
+        }
+        public static bool ShouldMoveFile(string file)
+        {
+            return File.Exists(file) && !IsIgnoredFile(file);
+        }
+        public static bool ShouldMoveFileToNewDirection(string file, string directory)
+        {
+            return File.Exists(file) && Directory.Exists(directory);
+        }
+        public static string RemoveDiscInfo(string fileName)
+        {
+            return Regex.Replace(fileName, DiscInfoPattern, "");
+        }
+        public static bool IsIgnoredFile(string path)
+        {
+            string fileName = Path.GetFileName(path);
+            return fileName.Equals(Program.IgnoreFile, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
